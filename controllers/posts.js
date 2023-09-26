8dee138dd026f02736ac981bf4ae969925b282ba
ducanhute <anhhdtech@gmail.com>
2023-09-26 10:46:29 +0700

    Done pagination

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,14 +2,35 @@ import mongoose from "mongoose";
 import PostMessage from "../models/postMessages.js";
 
 export const getPost = async (req, res) => {
-    try {
-        const postMessages = await PostMessage.find();
+    const { page } = req.query;
 
-        res.status(200).json(postMessages);
+    try {
+        const LIMIT = 4;
+        const startIndex = (Number(page) - 1) * LIMIT; // starting index of every page
+        const total = await PostMessage.countDocuments({}); // total number of documents in database
+        // Sort: newest to oldest: -1 give us the newest first, and fetch 8 post, skip startIndex posts(page2: get 8-16)
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+
+        res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
     } catch (error) {
-        res.send(404).json({ message: error.message });
+        res.status(404).json({ message: error.message });
     }
 };
+
+export const getPostsBySearch = async (req, res) => {
+    const { searchQuery, tags } = req.query;
+    try {
+        const title = new RegExp(searchQuery, "i"); // Test. test. TEST -> test
+        const posts = await PostMessage.find({ $or: [{ title }, { tags: { $in: tags.split(",") } }] });
+        res.json({ data: posts });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ message: error.message });
+    }
+};
+
+// query -> /posts?page=1 -> page =1
+// params -> /posts/123  -> id = 123
 
 export const createPost = async (req, res) => {
     const post = req.body;
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { getPost, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
+import { getPostsBySearch, getPost, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
+router.get("/search", getPostsBySearch);
 router.get("/", getPost);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
